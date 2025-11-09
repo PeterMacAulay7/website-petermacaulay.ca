321e6c81eed60d85468f5a8ccd285dda434d7204
PeterMacAulay7 <pjmacaulay@mta.ca>
2025-11-09 17:18:15 -0400

    update music on repeat

--- imports/left-sidebar.php
+++ imports/left-sidebar.php
@@ -18,6 +18,10 @@
             <li>West End Girl -Lily Allen</li>
             <li>Let God Sort Em Out -Clipse</li>
             <li>We got it from Here... Thank You 4 Your Service -A Tribe Called Quest</li>
+            <li>God Does Like Ugly -J.I.D</li>
+            <li>Alfredo2 -Freddy Gibbs</li>
+            <li>Don't Tap The Glass -Tyler The Creator</li>
+            <li>Call Me If You Get Lost -Tyler The Creator</li>
           </ul>
         </div>
 
