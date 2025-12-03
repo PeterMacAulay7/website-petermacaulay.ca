228ab02f6fcf17277a608811c73ec9e1801a1b3b
PeterMacAulay7 <pjmacaulay@mta.ca>
2025-12-02 23:32:18 -0400

    changes to home page: why I made this

--- imports/home.php
+++ imports/home.php
@@ -17,14 +17,22 @@
 
     <section>
       <h2>Why I Made This</h2>
-        <p>I’ve always wanted a space to learn, build, and reflect in public — 
-          this site is where I’m figuring that out. Everything here is handmade and in progress.</p>
+        <p>This is place for me to learn web development while simulataneously giving me a place to host my essays, projects, blog posts, or anything I want to share with the world.<br>
+          You can learn more about my motivation behind this website by perusing the actual site. That being said, the short of it is that I think a project like building a website gives me an 
+          excuse to create things I care about and actually share them with the world in the way I want to.<br>
+          It allows me to have a stronger foundation for continued growth; otherwise it feels like building on top of a quick sand tower. By having my previous work released to the world 
+          it solidly cements it in place, giving a strong foundation to build that tower on. This way I can feel more connected with the greater world around me, because I now have something to point to; 
+          I am no longer incoherently babbling about things that are only in my head.<br>
+          My hope is that others will follow suit by creating their own websites. I really think it's a valuable exercise that can provide you with something that you may be looking for, even if you don't know it yet.</p>
     </section>
   </div>
 
   <div class="outter">
     <h2>Most Recent:</h2>
     <nav>
+      <h3><a href = "?nav=projects">Latest Project</a></h3>
+      <?php echo get_first_div('imports/projects.php', 'contentbox'); ?>
+
       <h3><a href = "?nav=blog">Latest Blog Post</a></h3>
       <?php
       $latest = get_latest_post_by_internal_date('blogposts');
@@ -36,9 +44,6 @@
       $latest = get_latest_post_by_internal_date('essayfolder');
       echo $latest ? get_first_div($latest, 'contentbox') : "<p>No essays yet.</p>";
       ?>
-
-      <h3><a href = "?nav=projects">Latest Project</a></h3>
-      <?php echo get_first_div('imports/projects.php', 'contentbox'); ?>
     </nav>
   </div>
 
