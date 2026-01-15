e25d49615d62c793e74c2ce4b491317c3571cacc
PeterMacAulay7 <pjmacaulay@mta.ca>
2026-01-14 22:31:50 -0400

    Changed how sidebars are styled e.g., boarder around each section of the sidebars, and improved CSS management for sidebars. Also added new section for last Git commit message on the right sidebar

--- imports/left-sidebar.php
+++ imports/left-sidebar.php
@@ -1,52 +1,63 @@
-      <div class = "sidebar">
-        <h2>
-          Currently Reading:
-        </h2>
-        <div class = "scroll-box">
-          <ul>
-            <li>Circe by Madeline Miller</li><br>
-            <li>Little Women by Louisa May Alcott </li>
-          </ul>
-          <h3>readings for class</h3>
-          <ul>
-            <li>Unspeakable things -Silvia Jonas&check;</li><br>
-            <li>Aesthetic ineffability -Silvia Jonas&check;</li><br>
-            <li>Ineffability: the Very Concept -Sebastian Gäb</li><br>
-            <li>What Is Ineffable? -Jan Zwicky</li><br>
-            <li>Meno -Plato</li><br>
-            <li>The Mystical Theology -Dionysius</li><br>
-            <li>Lectures on Ethics -Wittgenstein</li><br>
-          </ul>
-        </div>
+<div class="sidebar">
 
-        <h2>
-          Currently On Repeat And Favourites:
-        </h2>
-        <div class = "scroll-box">
-          <ul>
-            <li>R.A.P. Music -Killer Mike</li><br>
-            <li>Magic,Alive! -McKinley Dixon</li><br>
-            <li>Cabin In The Sky -De La Soul</li><br>
-            <li>Voodoo -D'Angelo</li><br>
-            <li>Unoriginal/Black-Eyed Susan Climb -Magdalena Bay</li><br>
-            <li>West End Girl -Lily Allen</li><br>
-            <li>Let God Sort Em Out -Clipse</li><br>
-            <li>We got it from Here... Thank You 4 Your Service -A Tribe Called Quest</li><br>
-            <li>To Pimp A Butterfly -Kendrick Lamar &#40Always a classic&#41</li><br>
-            <li>God Does Like Ugly -J.I.D</li><br>
-            <li>Stankonia -Outkast</li><br>
-            <li>Alfredo2 -Freddy Gibbs</li><br>
-            <li>Blonde -Frank Ocean</li><br>
-            <li>Don't Tap The Glass -Tyler The Creator</li><br>
-            <li>Call Me If You Get Lost -Tyler The Creator</li><br>
-          </ul>
-        </div>
-        <h3>Please Give me recommendations, I'm always looking for new music. I can also give you recommendations if you want.</h3>
+  <!-- Currently Reading -->
+  <div class="sidebar-section">
+    <h2>Currently Reading:</h2>
+    <div class="scroll-box">
 
+      <h3>Personal</h3>
+      <ul>
+        <li>Circe — Madeline Miller</li>
+        <li>Little Women — Louisa May Alcott</li>
+      </ul>
 
-        <h2>Friend&#39s Websites</h2>
-        <nav>
-          <a href="https://bennettbeaumont.com"><button>Ben Beaumont <img src="/bennettbeaumont.com/TheIcon.webp" style="width:20px;height:auto;"></button></a>
-        </nav>
-        
-      </div>
+      <h3>Readings for Class</h3>
+      <ul>
+        <li>Unspeakable Things —Silvia Jonas ✓</li>
+        <li>Aesthetic Ineffability —Silvia Jonas ✓</li>
+        <li>Ineffability: the Very Concept —Sebastian Gäb</li>
+        <li>What Is Ineffable? —Jan Zwicky</li>
+        <li>Meno —Plato</li>
+        <li>The Mystical Theology —Dionysius</li>
+        <li>Lectures on Ethics —Wittgenstein</li>
+      </ul>
+
+    </div>
+  </div>
+
+  <!-- Music -->
+  <div class="sidebar-section">
+    <h2>Currently On Repeat & Favourites:</h2>
+    <div class="scroll-box">
+      <ul>
+        <li>R.A.P. Music — Killer Mike</li>
+        <li>Magic, Alive! — McKinley Dixon</li>
+        <li>Cabin in the Sky — De La Soul</li>
+        <li>Voodoo — D'Angelo</li>
+        <li>Unoriginal / Black-Eyed Susan Climb — Magdalena Bay</li>
+        <li>West End Girl — Lily Allen</li>
+        <li>Let God Sort Em Out — Clipse</li>
+        <li>We Got It from Here… Thank You 4 Your Service — A Tribe Called Quest</li>
+        <li>To Pimp a Butterfly — Kendrick Lamar (always a classic)</li>
+        <li>God Does Like Ugly — J.I.D</li>
+        <li>Stankonia — Outkast</li>
+        <li>Alfredo 2 — Freddie Gibbs</li>
+        <li>Blonde — Frank Ocean</li>
+        <li>Don’t Tap the Glass — Tyler, The Creator</li>
+        <li>Call Me If You Get Lost — Tyler, The Creator</li>
+      </ul>
+    </div>
+
+    <p class="sidebar-note">
+      Please give me recommendations I’m always looking for new music.
+      I can also give you recommendations if you want.
+    </p>
+  </div>
+
+  <!-- Friends -->
+  <div class="sidebar-section">
+    <h2>Friends’ Websites</h2>
+    <nav> <a href="https://bennettbeaumont.com"><button>Ben Beaumont <img src="/bennettbeaumont.com/TheIcon.webp" style="width:20px;height:auto;"></button></a> </nav>
+  </div>
+
+</div>
--- imports/right-sidebar.php
+++ imports/right-sidebar.php
@@ -1,44 +1,86 @@
-<link rel="stylesheet" href="style.css">
-</div>
-      <div class = "sidebar">
-        <h2>
-            Status Update:
-          </h2>
-        <div class = "scroll-box">
-          <ul>
-            <li>I just posted my latest project that I was working on in the fall semester. It's a group project that was created as a term project in my Software Design class</li><br>
-            <li>Just posted a blog post that I'm actually pretty happy with for a quick little post. I&#39ve found that these things really help me know what I&#39m thinking about, so that irl I have something to back up my thoughts, and I think it will long term help to practice writing</li><br>
-            <li>I have an essay and a project coming soon. They are both school assignments so I will likely add them mid December after exams</li><br>
-            <li>Added functionality for displaying content in Blog, Essays, and Projects. I also created fun little images for a couple of projects and an essay. Additionally I wrote a function that can scrap the most recent of each page to display on the home page. -Nov/04/25</li>
-            <br>
-            <li>Just updated the page to use the index.php file as a directory for all other pages. 
-              It took a lot longer than expected. &#40I guess thats to be expected&#41 -Nov/03/25</li>
-            <br>
-            <li>Added a 404 page with amusing photos -Nov/03/25</li>
-            <br>
-            <li>Currently learning HTML/CSS/PHP</li>
-            <br>
-            <li>I have big plans for taking the dev side of this website to a new level;
-              you may not see all of it as a visitor, I will have a blog post or project update when I'm done.
-              Mostly because those are the types of pages that will be possible with my plans.
-            </li>
-          </ul>
-        </div>
-        <h2>
-          Next Improv Show:
-        </h2>
-        <div class = "scroll-box">
-          <ul>
-            <li>Great News! MtA Presents: The Improv is back for 2026!</li><br>
-            <li>Shows will now be on Mondays at 9:30 @ The Pond</li>
-          </ul>
-        </div>
-        <h2>
-          TODO:
-        </h2>
-        <div class = "scroll-box">
-          <ul>
-            <li>Survive This Semester</li>
-            <br>
-          </ul>
+<div class="sidebar">
+
+  <!-- Next Improv Show -->
+  <div class="sidebar-section">
+    <h2>Next Improv Show:</h2>
+    <div class="scroll-box">
+      <ul>
+        <li>Great News! MtA Presents: The Improv is back for 2026!</li>
+        <li>Shows will now be on Mondays at 9:30 @ The Pond.</li>
+      </ul>
     </div>
+  </div>
+
+  <!-- Last GitHub Commit -->
+  <div class="sidebar-section">
+    <h2>Last GitHub Commit:</h2>
+    <div class ="scroll-box">
+      <ul class="sidebar-list">
+        <?php
+          $url = "https://api.github.com/repos/PeterMacAulay7/website-petermacaulay.ca/commits?per_page=1";
+
+          $options = [
+            "http" => [
+              "method" => "GET",
+              "header" => "User-Agent: petermacaulay.ca\r\n"
+            ]
+          ];
+
+          $context = stream_context_create($options);
+          $response = file_get_contents($url, false, $context);
+
+          if ($response !== false) {
+            $commits = json_decode($response, true);
+
+            if (!empty($commits)) {
+              $latest = $commits[0];
+              $message = htmlspecialchars($latest["commit"]["message"]);
+              $date = date("M j, Y", strtotime($latest["commit"]["author"]["date"]));
+              $link = $latest["html_url"];
+
+              echo "<li>";
+              echo "<a href='$link' target='_blank'>$message</a>";
+              echo "<small>$date</small>";
+              echo "</li>";
+            }
+          }
+        ?>
+      </ul>
+    </div>
+  </div>
+
+  <!-- Status Update -->
+  <div class="sidebar-section">
+    <h2>Status Update:</h2>
+    <div class="scroll-box">
+      <ul>
+        <li>I just posted my latest project that I was working on in the fall semester. It's a group project that was created as a term project in my Software Design class.</li>
+
+        <li>Just posted a blog post that I'm actually pretty happy with for a quick little post. I've found that these things really help me know what I'm thinking about, so that IRL I have something to back up my thoughts, and I think it will long term help to practice writing.</li>
+
+        <li>I have an essay and a project coming soon. They are both school assignments so I will likely add them mid December after exams.</li>
+
+        <li>Added functionality for displaying content in Blog, Essays, and Projects. I also created fun little images for a couple of projects and an essay. Additionally I wrote a function that can scrape the most recent of each page to display on the home page. – Nov/04/25</li>
+
+        <li>Just updated the page to use the index.php file as a directory for all other pages. It took a lot longer than expected (I guess that's to be expected). – Nov/03/25</li>
+
+        <li>Added a 404 page with amusing photos – Nov/03/25</li>
+
+        <li>Currently learning HTML/CSS/PHP.</li>
+
+        <li>I have big plans for taking the dev side of this website to a new level; you may not see all of it as a visitor. I will have a blog post or project update when I'm done, mostly because those are the types of pages that will be possible with my plans.</li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- TODO -->
+  <div class="sidebar-section">
+    <h2>TODO:</h2>
+    <div class="scroll-box">
+      <ul>
+        <li>Survive This Semester</li>
+      </ul>
+    </div>
+  </div>
+
+</div>
