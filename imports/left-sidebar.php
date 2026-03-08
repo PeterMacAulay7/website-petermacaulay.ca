a51257e28c6edfbbd2d2dcef56a6a8d828fac5a0
PeterMacAulay7 <pjmacaulay@mta.ca>
2026-03-08 16:13:01 -0300

    updated currently on repeat in left sidebar

--- imports/left-sidebar.php
+++ imports/left-sidebar.php
@@ -4,32 +4,23 @@
   <div class="sidebar-section">
     <h2>Currently Reading:</h2>
     <div class="scroll-box">
-
       <h3>Personal</h3>
       <ul>
         <li>Circe — Madeline Miller</li>
         <li>Little Women — Louisa May Alcott - On Hold for now</li>
       </ul>
 
-      <h3>Sources</h3>
+      <h3>Sources For Incommunicability</h3>
       <ul>
         <li>Black Skin, White Masks - Frantz Fanon</li>
         <li>What is Lyric Philosophy - Jan Zwicky</li>
         <li>Meno - Plato</li>
       </ul>
 
-      <h3>Readings for Class</h3>
-      <ul>
-        <li>Unspeakable Things - Silvia Jonas ✓</li>
-        <li>Aesthetic Ineffability - Silvia Jonas ✓</li>
-        <li>Ineffability: the Very Concept - Sebastian Gäb</li>
-        <li>What Is Ineffable? - Jan Zwicky✓</li>
-        <li>Meno - Plato✓&#9733;</li>
-        <li>The Mystical Theology - Dionysius✓&#9733;</li>
-        <li>Lectures on Ethics - Wittgenstein✓&#9733;</li>
-      </ul>
-
     </div>
+    <p class="sidebar-note">
+      You can see my <a href="/books">full book library here</a>
+    </p>
   </div>
 
   <!-- Music -->
@@ -38,24 +29,21 @@
     <p class="sidebar-note"></p>Get an MP3 player, they're cool and fun</p>
     <div class="scroll-box">
       <ul>
+        <li>To Whom This May Concern - Jill Scott</li>
+        <li>The Apple Tree Under the Sea - Hemlocke Springs</li>
         <li>The Cool - Lupe Fiasco</li>
         <li>R.A.P. Music - Killer Mike</li>
         <li>e.t.d.s - IDK</li>
         <li>Magic, Alive! - McKinley Dixon</li>
-        <li>Voodoo - D'Angelo</li>
         <li>Resurrection - Common</li>
         <li>Momma's Gun- Erykah Badu</li>
-        <li>Run The Jewels I, II, III, IV, IV CU4TRO - Run The Jewels</li>
         <li>Let God Sort Em Out - Clipse</li>
-        <li>Things Fall Apart - The Roots</li>
-        <li>The velvet Rope - Janet Jackson</li>
-        <li>Maps - Billy Woods and Kenny Segal</li>
       </ul>
     </div>
 
     <p class="sidebar-note">
       Please give me recommendations I’m always looking for new music.
-      I can also give you recommendations if you want. Or you can check out my <a href="music.php">music library</a>
+      I can also give you recommendations if you want. Or you can check out my <a href="/music">music library here</a>
     </p>
   </div>
 
