5e56fcf4541725602d6c0b748882c47d4ab4ff08
PeterMacAulay7 <pjmacaulay@mta.ca>
2026-05-07 16:14:31 -0300

    added line break for date of last git commit, and removed status update and TODO to align the homepage and sidebars with goal of giving a glimpse into what the rest of the site offers

--- imports/right-sidebar.php
+++ imports/right-sidebar.php
@@ -48,7 +48,7 @@ $recentMovies = getRecentMovies(3); // or 2–3
 
               echo "<li>";
               echo "<a href='$link' target='_blank'>$message </a>";
-              echo "<small>$date</small>";
+              echo "<small><br>$date</small>";
               echo "</li>";
             }
           }
@@ -75,39 +75,5 @@ $recentMovies = getRecentMovies(3); // or 2–3
     You can see my <a href="/movies">full movie library here</a>
   </p>
 </div>
-  
-  <!-- Status Update -->
-  <div class="sidebar-section">
-    <h2>Status Update:</h2>
-    <div class="scroll-box">
-      <ul>        
-        <li>I just posted my latest project that I was working on in the fall semester. It's a group project that was created as a term project in my Software Design class.</li>
-
-        <li>Just posted a blog post that I'm actually pretty happy with for a quick little post. I've found that these things really help me know what I'm thinking about, so that IRL I have something to back up my thoughts, and I think it will long term help to practice writing.</li>
-
-        <li>I have an essay and a project coming soon. They are both school assignments so I will likely add them mid December after exams.</li>
-
-        <li>Added functionality for displaying content in Blog, Essays, and Projects. I also created fun little images for a couple of projects and an essay. Additionally I wrote a function that can scrape the most recent of each page to display on the home page. – Nov/04/25</li>
-
-        <li>Just updated the page to use the index.php file as a directory for all other pages. It took a lot longer than expected (I guess that's to be expected). – Nov/03/25</li>
-
-        <li>Added a 404 page with amusing photos – Nov/03/25</li>
-
-        <li>Currently learning HTML/CSS/PHP.</li>
-
-        <li>I have big plans for taking the dev side of this website to a new level; you may not see all of it as a visitor. I will have a blog post or project update when I'm done, mostly because those are the types of pages that will be possible with my plans.</li>
-      </ul>
-    </div>
-  </div>
-
-  <!-- TODO -->
-  <div class="sidebar-section">
-    <h2>TODO:</h2>
-    <div class="scroll-box">
-      <ul>
-        <li>Survive This Semester</li>
-      </ul>
-    </div>
-  </div>
 
 </div>
