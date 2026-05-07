4d7718e5a0015ad89d2ac1b4b31d62f0b9e05b3d
PeterMacAulay7 <pjmacaulay@mta.ca>
2026-05-07 16:17:23 -0300

    revised about, will need to do more in the future though

--- imports/about.php
+++ imports/about.php
@@ -31,38 +31,24 @@
               <li>Philosophy</li>
               <li>Programming</li>
               <li>Reading</li>
-              <li>Writing</li>
-              <li>Spending time with my girlfriend <a href="/?nav=nadia">Nadia</a></li>
+              <li>Writing / Journalling</li>
+              <li>Spending time with my girlfriend, <a href="/?nav=nadia">Nadia</a></li>
               <li>Spending time with friends</li>
-              <li>and more!</li>
+              <li>and much more!</li>
             </ul>
             <img src="/images/PeterPhoto.webp" alt="PeterPhoto" style="width:200px;height:auto;">
         </div>
-      <section>
-        <p>
-          I'm not really sure how to start this page, I just want to give a short-ish paragraph about who I am. All my life, I've loved making
-          and understanding things. While I haven't always been the best at either of those, they're aspects of my lfie that have always
-          been present. Learning, and learning how I learn, is something that seems to be at the center of those two aspect of my life. I have many
-          interests (as stated above), and a lot of them are very different. These interests have allowed me to see the world in different ways. It 
-          has given me the chance to see the world more clearly than I ever thought. I said above that learning seems to be at the center of making
-          and understanding, but I think that those may be only a means to keep learning. Learning is what I think keeps you alive. The day you stop
-          learning is that day you die. By virtue of making and seeking understanding, I am able to keep learning.
-      </p>
-      <p>
-          This website is soemthing that I've wanted to make for a while, and it wasn't until I started to see some people that I'm interested
-          in, talking about a more human internet, where everyone makes their own websites, and shares with the world their creations, thoughts, and life 
-          that I decided to actually do it. For a while I was stuck. I didn't know what mattered to me, and I didn't know what I needed to do.
-          Once I started to find my values, and goals, I was able to start seeing a bit clearer. I was able to start seeking understanding again.
-          I started learning again. But something was missing, I still couldn't create; I couldn't create much more than something on a whim in a rush of
-          inspiration. This was good, but I needed more. I decided that I would continue seeking understanding, with the hope that creation would follow.
-          Eventually, as I mentioned, I found something. I think that this website has the potnential to allow me to create, and share with the world, while
-          simultaneously allowing me to have a reason to engage in other projects and interests. I may be optimistic, but this feels like the epicenter of 
-          something that can allow me to engage in something larger than myself while also allowing me to grow as a person.
-      </p>
 
-      <p>
-        Thanks for reading! I'll have more to come soon. :D -Oct 16, 2025
-      </p>
+        <section>
+        <h2>Why I Made This</h2>
+        <p>
+          You can learn more about my motivation behind this website by perusing the actual site. That being said, the short of it is that I think a project like building a website gives me an 
+          excuse to create things I care about and actually share them with the world in the way I want to.
+          It allows me to have a stronger foundation for continued growth; otherwise it feels like building on top of a quick sand tower. By having my previous work released to the world 
+          it solidly cements it in place, giving a strong foundation to build that tower on. This way I can feel more connected with the greater world around me, because I now have something to point to; 
+          I am no longer incoherently babbling about things that are only in my head.
+          My hope is that others will follow suit by creating their own websites. I really think it's a valuable exercise that can provide you with something that you may be looking for, even if you don't know it yet.
+        </p>
       </section>
     </div>
   </body>
