6a41ae909b0287989e85424a2f1c60e8d0989760
PeterMacAulay7 <petermacaulay7@outlook.com>
2026-09-02 22:12:10 -0300

    updated interests

--- imports/about.php
+++ imports/about.php
@@ -18,37 +18,44 @@
   </style>
 
   <body>
-    <div>
-      <p>
+    <div class="contentbox text-only">
+      <div class="text">
+        <p>
           Hello, my name is Peter MacAulay. I'm currently a fourth year student at Mount Allison University. I'm majoring in Computer Science and minoring in Philosophy and Mathematics.
         </p>
-        <div class ="intro">
-        Some of my interests include:
-        </div>
+      </div>
+    </div>
+    <h2>
+      Some of my interests include:
+    </h2>
         <div class="contentbox">
             <ul>
               <li>Improv Comedy</li>
               <li>Philosophy</li>
               <li>Programming</li>
               <li>Reading</li>
+              <li>Camping</li>
+              <li>Working on my Homelab</li>
               <li>Writing / Journalling</li>
               <li>Spending time with my girlfriend, <a href="/?nav=nadia">Nadia</a></li>
-              <li>Spending time with friends</li>
-              <li>and much more!</li>
             </ul>
             <img src="/images/PeterPhoto.webp" alt="PeterPhoto" style="width:200px;height:auto;">
-        </div>
+      </div>
 
         <section>
         <h2>Why I Made This</h2>
-        <p>
-          You can learn more about my motivation behind this website by perusing the actual site. That being said, the short of it is that I think a project like building a website gives me an 
-          excuse to create things I care about and actually share them with the world in the way I want to.
-          It allows me to have a stronger foundation for continued growth; otherwise it feels like building on top of a quick sand tower. By having my previous work released to the world 
-          it solidly cements it in place, giving a strong foundation to build that tower on. This way I can feel more connected with the greater world around me, because I now have something to point to; 
-          I am no longer incoherently babbling about things that are only in my head.
-          My hope is that others will follow suit by creating their own websites. I really think it's a valuable exercise that can provide you with something that you may be looking for, even if you don't know it yet.
-        </p>
+        <div class="contentbox text-only">
+          <div class="text">
+            <p>
+              You can learn more about my motivation behind this website by perusing the actual site. That being said, the short of it is that I think a project like building a website gives me an 
+              excuse to create things I care about and actually share them with the world in the way I want to.
+              It allows me to have a stronger foundation for continued growth; otherwise it feels like building on top of a quick sand tower. By having my previous work released to the world 
+              it solidly cements it in place, giving a strong foundation to build that tower on. This way I can feel more connected with the greater world around me, because I now have something to point to; 
+              I am no longer incoherently babbling about things that are only in my head.
+              My hope is that others will follow suit by creating their own websites. I really think it's a valuable exercise that can provide you with something that you may be looking for, even if you don't know it yet.
+            </p>
+          </div>
+        </div>
       </section>
     </div>
   </body>
